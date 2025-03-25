3a74a424ff3fade857d346178ed8130d6a36b0b2
Charandeepak1234 <167806260+Charandeepak1234@users.noreply.github.com>
2025-03-25 18:06:11 +0530

    Update tutorial.js

--- pages/lessons/tutorial.js
+++ pages/lessons/tutorial.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
 import styles from "/styles/typing.module.css"; // Import the CSS Module
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import lesson1 from "@/public/lesson1img.png"
+import lesson2 from "@/public/lesson2img.png"
+import lesson3 from "@/public/lesson3img.png"
+import lesson4 from "@/public/lesson4img.png"
+import lesson5 from "@/public/lesson5img.png"
+import lesson6 from "@/public/lesson6img.png"
+import Image from "next/image";
 
 
 const Typing = () => {
@@ -17,6 +24,12 @@ const Typing = () => {
         <li className={styles.container1}>
           <Link href="/lessons/tutorial/tutorial1">
             <div>
+              <Image
+                   src={lesson1}
+                   alt="Coding Lessons"
+                   width={200}
+                   height={200}
+              />
               <button className={styles.buttonElement}>Lesson 1</button>
             </div>
           </Link>
@@ -24,6 +37,12 @@ const Typing = () => {
         <li className={styles.container2}>
           <Link href="/lessons/tutorial/tutorial2">
             <div>
+              <Image
+                   src={lesson2}
+                   alt="Coding Lessons"
+                   width={200}
+                   height={200}
+              />
               <button className={styles.buttonElement}>Lesson 2</button>
             </div>
           </Link>
@@ -31,6 +50,12 @@ const Typing = () => {
         <li className={styles.container3}>
           <Link href="/lessons/tutorial/tutorial3">
             <div>
+              <Image
+                   src={lesson3}
+                   alt="Coding Lessons"
+                   width={200}
+                   height={200}
+              />
               <button className={styles.buttonElement}>Lesson 3</button>
             </div>
           </Link>
@@ -38,6 +63,12 @@ const Typing = () => {
         <li className={styles.container4}>
           <Link href="/lessons/tutorial/tutorial4">
             <div>
+              <Image
+                   src={lesson4}
+                   alt="Coding Lessons"
+                   width={200}
+                   height={200}
+              />
               <button className={styles.buttonElement}>Lesson 4</button>
             </div>
           </Link>
@@ -45,6 +76,12 @@ const Typing = () => {
         <li className={styles.container5}>
           <Link href="/lessons/tutorial/tutorial5">
             <div>
+              <Image
+                   src={lesson5}
+                   alt="Coding Lessons"
+                   width={200}
+                   height={200}
+              />
               <button className={styles.buttonElement}>Lesson 5</button>
             </div>
           </Link>
@@ -52,6 +89,12 @@ const Typing = () => {
         <li className={styles.container6}>
           <Link href="/lessons/tutorial/tutorial6">
             <div>
+              <Image
+                   src={lesson6}
+                   alt="Coding Lessons"
+                   width={200}
+                   height={200}
+              />
               <button className={styles.buttonElement}>Lesson 6</button>
             </div>
           </Link>
@@ -59,6 +102,12 @@ const Typing = () => {
         <li className={styles.container1}>
           <Link href="/lessons/tutorial/tutorial7">
             <div>
+              <Image
+                   src={lesson1}
+                   alt="Coding Lessons"
+                   width={200}
+                   height={200}
+              />
               <button className={styles.buttonElement}>Lesson 7</button>
             </div>
           </Link>
@@ -66,6 +115,12 @@ const Typing = () => {
         <li className={styles.container1}>
           <Link href="/lessons/tutorial/tutorial8">
             <div>
+              <Image
+                   src={lesson3}
+                   alt="Coding Lessons"
+                   width={200}
+                   height={200}
+              />
               <button className={styles.buttonElement}>Lesson 8</button>
             </div>
           </Link>
